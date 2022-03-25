4d966be1b31364e180f1741f475a86d241410b38
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-25 07:17:23 +0300

    input validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,27 @@
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
 const User = require("../models/user");
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
     // const email = args.userInput.email;
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "Invalid email address" });
+    }
+
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: "Password must be at least 5 characters long" });
+    }
+
+    if (errors.length > 0) {
+      const error = new Error("Invalid input!");
+      throw error;
+    }
 
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
