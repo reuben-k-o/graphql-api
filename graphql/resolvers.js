11432636ca40651de20dea52214eb59006819233
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-30 08:38:23 +0300

    saving correctly created post

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -108,6 +108,8 @@ module.exports = {
 
     const createdPost = await post.save();
     user.posts.push(createdPost);
+    await user.save();
+
     return {
       ...createdPost._doc,
       _id: createdPost._id.toString(),
